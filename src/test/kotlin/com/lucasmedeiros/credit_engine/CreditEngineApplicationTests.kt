7ffd8058cd289199b4c2f608f791d5369a21dc3a
l-medeiros <lucas.santos@fluencyacademy.io>
2025-07-25 16:51:16 -0300

    Initial commit

---
+++ src/test/kotlin/com/lucasmedeiros/credit_engine/CreditEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lucasmedeiros.credit_engine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
